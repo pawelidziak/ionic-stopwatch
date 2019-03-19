7cc6413c24d492e935278ce5384192ec74cdb03f
pIdziak <pa.idziak@gmail.com>
2019-03-19 18:01:44 +0100

    create simple circle view

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,114 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  percent: number = 0;
+  radius: number = 100;
+  fullTime: any = '00:01:30';
+  timeLeft: any = {
+    m: '00',
+    s: '00'
+  };
+  remainingTime = `${this.timeLeft.m}:${this.timeLeft.s}`;
+  timer: any = false;
+  progress: any = 0;
+  minutes: number = 1;
+  seconds: any = 10;
+
+  elapsed: any = {
+    h: '00',
+    m: '00',
+    s: '00'
+  };
+  overallProgress: any = 0;
+  overallTimer: any = false;
+
+
+  countDownTimer: any = false;
+
+
+  touchMe() {
+    console.log('touched');
+  }
+
+  startTimer() {
+
+    if(this.timer) {
+      clearInterval(this.timer);
+      clearInterval(this.countDownTimer);
+    }
+    if(!this.overallTimer) {
+      this.progressTimer();
+    }
+
+    this.timer = false;
+    this.percent = 0;
+    this.progress = 0;
+
+    let timeSplit = this.fullTime.split(':');
+    this.minutes = timeSplit[1];
+    this.seconds = timeSplit[2];
+
+    let totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
+    let secondsLeft = totalSeconds;
+
+    let forwardsTimer = () => {
+      if (this.percent == this.radius) clearInterval(this.timer)
+      this.percent = Math.floor((this.progress / totalSeconds) * 100)
+      ++this.progress
+    }
+
+    let backwardsTimer = () => {
+      if (secondsLeft >= 0) {
+        this.timeLeft.m = Math.floor(secondsLeft / 60)
+        this.timeLeft.s = secondsLeft - (60 * this.timeLeft.m)
+        this.remainingTime = `${this.pad(this.timeLeft.m, 2)}:${this.pad(this.timeLeft.s, 2)}`
+        secondsLeft--;
+      }
+    }
+
+    // run once when clicked
+    forwardsTimer()
+    backwardsTimer()
+
+    // timers start 1 second later
+    this.countDownTimer = setInterval(backwardsTimer, 1000)
+    this.timer = setInterval(forwardsTimer, 1000)
+  }
+
+  stopTimer() {
+  }
+
+  progressTimer() {
+    let countDownDate = new Date();
+
+    this.overallTimer = setInterval(() => {
+      let now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      let distance = now - countDownDate.getTime();
+
+      // Time calculations for hours, minutes and seconds
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.elapsed.h = this.pad(this.elapsed.h, 2);
+      this.elapsed.m = this.pad(this.elapsed.m, 2);
+      this.elapsed.s = this.pad(this.elapsed.s, 2);
+
+
+
+    },1000)
+  }
+
+  pad(num, size) {
+    let s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
+
+  updateMyDate($event) {
+    console.log($event.split(":"));
+  }
 
 }
